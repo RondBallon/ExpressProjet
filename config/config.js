6f28eb29913303fd32c27a7e80c2a923daf5e238
CeliaLachenal <59375489+CeliaLachenal@users.noreply.github.com>
2024-12-05 12:05:38 +0100

    fix config.js

--- config/config.js
+++ config/config.js
@@ -1,23 +1,24 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "postgres",
+    database: "blog",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    logging: console.log, // Optionnel : pour afficher les requêtes SQL
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "postgres",
+    password: "postgres",
+    database: "blog",
+    host: "127.0.0.1",
+    dialect: "postgres",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: "postgres",
+    password: "postgres",
+    database: "blog",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
