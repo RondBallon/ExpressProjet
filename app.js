2de52e2c63fc4e78f0d2f0ed18919c6e9b412e49
Paul Vaucher <vaucher.paul@gmx.com>
2024-12-06 15:43:04 +0100

    ajouts cucumber dans scripts du package. BIGINT to INTEGER ds post model

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public'))); // Sert les fichiers st
 // Déclaration des routes
 app.use('/', indexRouter); // Utilise `indexRouter` pour la route racine `/`.
 app.use('/users', usersRouter); // Utilise `usersRouter` pour la route `/users`.
-app.use('/posts', postsRouter);
+app.use('/post', postsRouter);
 
 // Gestion des erreurs 404
 app.use(function(req, res, next) {
