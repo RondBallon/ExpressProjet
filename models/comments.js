a1e63599d367a9cabdd5dceaca80134a268b7282
CeliaLachenal <59375489+CeliaLachenal@users.noreply.github.com>
2024-12-11 14:22:00 +0100

    possibilité d'afficher un commentaire en particulier + ajout des boutons modifier et supprimé mais boutons non fonctionnels

--- models/comments.js
+++ models/comments.js
@@ -1,35 +1,43 @@
-'use strict';
-const {Model} = require('sequelize');
-const sequelize =require('../sequelize');
-const {DataTypes} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+const sequelize = require("../sequelize");
+const { DataTypes } = require("sequelize");
+/* const { primaryKey } = require("../sequelize"); */
 //module.exports = (sequelize, DataTypes) => {
-  class Comments extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Comments.init({
+class Comments extends Model {
+  /**
+   * Helper method for defining associations.
+   * This method is not a part of Sequelize lifecycle.
+   * The `models/index` file will call this method automatically.
+   */
+  //static associate(models) {
+  // define association here
+  //}
+}
+Comments.init(
+  {
+    /*    id: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+    }, */
     user_id: {
       type: DataTypes.BIGINT,
       allowNull: true,
-  }, // Permet des valeurs nulles pour user_id
-    post_id:{ 
-      type:DataTypes.BIGINT,
+    }, // Permet des valeurs nulles pour user_id
+    post_id: {
+      type: DataTypes.BIGINT,
       allowNull: true, // Permet des valeurs nulles pour post_id
-  },  
-    content:{
-        type:DataTypes.STRING,
-        allowNull: false, // Le contenu est tjs requis
-  },  
-  }, {
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false, // Le contenu est tjs requis
+    },
+  },
+  {
     sequelize,
-    modelName: 'Comments',
-  });
-  //return Comments;
+    modelName: "Comments",
+  }
+);
+//return Comments;
 //};
 module.exports = Comments;
